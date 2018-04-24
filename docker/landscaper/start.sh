048060dd8878b2c1b7bbed6eadd2dd5288659517
Leonard Feehan <leonardx.feehan@intel.com>
2018-04-24 16:45:46 +0100

    updates to cater for newer docker-compose format and bolt issues

--- docker/landscaper/start.sh
+++ docker/landscaper/start.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
+# wait for neo4j
+
+neo_host=$1
+
+echo $neo_host
+
+sleep 4
+while ! curl -f $neo_host; do sleep 1; done
+echo "---found neo4j"
+sleep 8
+echo "starting landscaper"
 
 python landscaper.py
